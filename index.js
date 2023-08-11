f78372aa2ad8d84e50b838a5e2b3a0ac249ed929
Elshen000 <elshen.q@itbrains.az>
2023-08-11 14:52:37 +0400

    resume page elave etdim

---
+++ index.js
@@ -0,0 +1,13 @@
+// SkillBar Elements Move With Linear
+setTimeout(() => {
+    $('.education_footer_left_cards_skill_figma').css("width",'95%'),
+     $('.education_footer_left_cards_skill_photoshop').css("width",'100%'),
+    $('.education_footer_left_cards_skill_adobeills').css("width",'70%'),
+    $('.education_footer_left_cards_skill_adobexd').css("width",'60%'),
+    $('.education_footer_left_cards_skill_design').css("width",'90%'),
+    $('.education_footer_right_cards_skill_html').css("width",'100%'),
+    $('.education_footer_right_cards_skill_css').css("width",'95%'),
+    $('.education_footer_right_cards_skill_javascript').css("width",'70%'),
+    $('.education_footer_right_cards_skill_software').css("width",'60%'),
+    $('.education_footer_right_cards_skill_plugin').css("width",'90%')
+}, 2000);
